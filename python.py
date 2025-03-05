72b635b24d2f83912a0e7cb72d77e1ba1df59491
Jay Sivarajan <j.sivarajan@albynschool.org>
2025-03-05 15:04:56 +0000

    hi

---
+++ python.py
@@ -0,0 +1,7 @@
+# comment ignored by the computer
+print("hello")
+print("there")
+print("S3.3")
+yourname = input("enter your name:")
+print("oh wow ,hi"+yourname)
+print("did you say your name was",yourname)

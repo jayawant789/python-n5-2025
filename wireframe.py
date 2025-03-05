35b9466e22696f57e1bc407cfd504bf2b4db4e9d
Jay Sivarajan <J.Sivarajan@albynschool.org>
2025-03-05 15:28:59 +0000

    hihihi

---
+++ wireframe.py
@@ -0,0 +1,19 @@
+print("Second Half Action")
+print("==================")
+
+print("")
+
+print("Enter the home team half time score: _______")
+print("Enter the away team half time score: _______")
+
+print("")
+
+print("Enter the home team full time score: _______")
+print("Enter the away team full time score: _______")
+
+print("")
+
+print("The total number of goals scored after halftime is:")
+print("<<toalscore>>")
+
+
---
+++ wireframe3.py
@@ -0,0 +1,4 @@
+print("saturdays")
+print("==========")
+print("")
+input("enter the total number of weekdays for janruary first")
